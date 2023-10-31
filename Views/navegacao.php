b8efb9b07dc552d73790e48a759c364059defd00
GOA03 <91618139+GOA03@users.noreply.github.com>
2023-10-31 16:48:49 -0300

    Update navegacao.php

--- Views/navegacao.php
+++ Views/navegacao.php
@@ -5,16 +5,23 @@
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
+        <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
                 <a class="nav-link" href="/Trabalho2/receitas">Listar Receitas</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="/Trabalho2/receitas/adicionar">Adicionar Receita</a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link text-danger" href="/Trabalho2/login?action=logout"><i class="fas fa-sign-out-alt"></i> Logout</a>
-            </li>
+        </ul>
+        <ul class="navbar-nav">
+            <?php if(isset($_SESSION['autenticado']) && $_SESSION['autenticado'] === true): ?>
+                <li class="nav-item">
+                    <span class="nav-link"><i class="fas fa-user"></i> Bem-vindo, <?php echo $_SESSION['usuario_logado']; ?>!</span>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-danger" href="/Trabalho2/login?action=logout"><i class="fas fa-sign-out-alt"></i> Logout</a>
+                </li>
+            <?php endif; ?>
         </ul>
     </div>
 </nav>
