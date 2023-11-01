53922235a4c51ca911a945a0702bb45529bb89f4
GOA03 <91618139+GOA03@users.noreply.github.com>
2023-10-31 22:34:29 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
 
     use Pecee\SimpleRouter\SimpleRouter as Router;
 
-    
+    // - - - - - - - - - - - - - - - - - - - ( L O G I N ) - - - - - - - - - - - - - - - - - - 
+
     // Rota padrão redireciona para o login
     Router::get('/Trabalho2/', function() {
         // Se estiver autenticado, redireciona para a lista de receitas.
@@ -20,40 +21,52 @@
         require './views/login.view.php';
     });
 
-    // Rota para visualizar uma receita específica
-    Router::get('/Trabalho2/receitas/visualizar/{id}', 'ReceitasController@visualizar');
-    
-    // Rota para editar uma receita específica
-    Router::get('/Trabalho2/receitas/editar/{id}', 'ReceitasController@editar');
-
-    // Rotas de Login e Logout
+    // Rotas de Login
     Router::get('/Trabalho2/login', function() {
         require __DIR__ . '/views/login.view.php';
     });
 
-
+    // Rotas de autenticação de Login
     Router::post('/Trabalho2/login', 'LoginController@authenticate');
+
+    // - - - - - - - - - - - - - - - - - - ( L O G O U T ) - - - - - - - - - - - - - - - - - - 
+
     Router::get('/Trabalho2/logout', 'LoginController@logout');
 
-    // Rotas de listar as receitas
+    // - - - - - - - - - - - - - - - - ( V I S U A L I Z A R ) - - - - - - - - - - - - - - - - 
+
+    // Rota para visualizar uma receita específica
+    Router::get('/Trabalho2/receitas/visualizar/{id}', 'ReceitasController@visualizar');
+
+    // - - - - - - - - - - - - - - - - - - ( E D I T A R ) - - - - - - - - - - - - - - - - - -
+    
+    // Rotas para editar uma receita específica
+    Router::get('/Trabalho2/receitas/editar/{id}', 'ReceitasController@editar');
+    Router::post('/Trabalho2/receitas/editar/{id}', 'ReceitasController@editar');
+
+    // - - - - - - - - - - - - - - - - - - ( L I S T A R ) - - - - - - - - - - - - - - - - - -
+
+    // Rota de listar as receitas
     Router::get('/Trabalho2/receitas', function() {
         require './views/listar.view.php';
     });
+
+    // - - - - - - - - - - - - - - - - - ( A D I C I O N A R ) - - - - - - - - - - - - - - - -
 
     Router::get('/Trabalho2/receitas/adicionar', function() {
         require './views/adicionar.view.php';
     });
 
-
     Router::post('/Trabalho2/receitas/adicionar', 'ReceitasController@adicionar');
-    Router::post('/Trabalho2/receitas/editar/{id}', 'ReceitasController@editar');
 
-    ///*
+    // - - - - - - - - - - - - - - - - - - ( T E S T E ) - - - - - - - - - - - - - - - - - - -
+
     //Teste do professor :)
     Router::get('Trabalho2/ping', function() {
         echo "Pong!";
     });
-    //*/
+
+    // - - - - - - - - - - - - - - - - - - ( S T A R T ) - - - - - - - - - - - - - - - - - - -
 
     Router::start();
 
